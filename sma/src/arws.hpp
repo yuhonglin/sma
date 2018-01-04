481c43a89ec2b5c4b7f90817826d643f2561ae09
Honglin Yu <honglin.yu@anu.edu.au>
2018-01-04 14:56:42 +1100

    add ar with shift

---
+++ sma/src/arws.hpp
@@ -0,0 +1,134 @@
+// Define auto regression with shift term.
+// Currently, only support basic mode: create an arws model for each row
+#ifndef ARWS_H
+#define ARWS_H
+
+#include <vector>
+#include <iostream>
+#include <memory>
+#include <cmath>
+#include <stdexcept>
+#include <algorithm>
+
+#include "matrix.hpp"
+#include "term.hpp"
+
+
+class AutoRegWithShift : public Term
+{
+public:
+  AutoRegWithShift(Variable* v, Variable* p, Variable* s, const std::vector<int>& ls)
+    : var_(v), param_(p), shift_(s), lags_(ls)
+  {
+    if (    v->m() != p->m()
+	or  p->n() != ls.size()
+	or (s->m() != v->m() and s->n() != v->m())) {
+      throw std::invalid_argument("ARWS: incompatible inputs");
+    }
+
+    max_lag_ = *std::max_element(lags_.begin(), lags_.end());
+    
+    diff_.reset(new double[(var_->n() - max_lag_) * var_->m()]);
+
+    vars_.insert(v);
+    vars_.insert(p);
+    vars_.insert(s);
+  };
+  
+  virtual ~AutoRegWithShift() = default;
+  
+  virtual double func() {
+    double ret = 0.;
+    int base = var_->m()*max_lag_;
+    int diffiter = 0;
+    for (int j = 0; j < var_->n()-max_lag_; j++) {
+      for (int i = 0; i < var_->m(); i++) {
+	diff_[diffiter] = var_->data()[base+diffiter] - shift_->data()[i];
+
+	for (int k = 0; k < lags_.size(); k++) {
+	  diff_[diffiter] -= param_->data()[k*param_->m()+i]
+	    * var_->data()[(max_lag_+j-lags_[k])*var_->m()+i];
+	}
+
+	ret += std::pow(diff_[diffiter], 2);
+	
+	diffiter ++;
+      }
+    }
+
+    return 0.5*lambda_*ret;
+  }
+
+  virtual void   inc_grad(Variable* v, double* g) {
+    if (v == var_) {
+      int base = var_->m()*max_lag_;
+      int diffiter = 0;      
+      for (int j = 0; j < var_->n()-max_lag_; j++) {
+	for (int i = 0; i < var_->m(); i++) {
+	  int vi = base+j*var_->m()+i;
+	  g[vi] += lambda_ * diff_[diffiter];
+	  for(int k = 0; k < lags_.size(); k++) {
+	    g[vi - lags_[k]*var_->m()] -= lambda_ * diff_[diffiter] * param_->data()[k*param_->m()+i];
+	  }
+	  diffiter ++;
+	}
+      }
+    } else if (v == param_) {
+      int base = var_->m()*max_lag_;
+      int diffiter = 0;      
+      for (int j = 0; j < var_->n()-max_lag_; j++) {
+	for (int i = 0; i < var_->m(); i++) {
+	  for(int k = 0; k < lags_.size(); k++) {
+	    int pi = k*param_->m() + i;
+	    g[pi] -= lambda_ * diff_[diffiter]
+	      * var_->data()[(max_lag_+j-lags_[k])*var_->m() + i];
+	  }
+	  diffiter ++;
+	}
+      }
+    } else if (v == shift_) {
+      for (int i = 0; i < var_->m(); i++) {
+	for (int j = 0; j < var_->n()-max_lag_; j++) {
+	  g[i] -= diff_[j*var_->m()+i];
+	}
+      }
+    } else {
+      throw std::invalid_argument("ARWS: unknown variable");
+    }
+  }
+
+  std::shared_ptr<Variable> forecast(int l) {
+    auto ret = std::make_shared<Variable> (var_->m(), l, var_->name() + "_forcast");
+
+#define SMA_AR_GET_VALUE(A, B)						\
+    ( (B) < 0 ? (var_->data()[var_->m()*(var_->n()+(B)) + (A)])		\
+      : (ret->data()[ret->m()*(B) + (A)]) )				\
+
+    for (int i = 0; i < ret->m(); i++) {
+      for (int j = 0; j < ret->n(); j++) {
+	ret->data()[j*ret->m() + i] = 0;
+	for (int k = 0; k < lags_.size(); k++) {
+	  ret->data()[j*ret->m() + i] +=
+	    SMA_AR_GET_VALUE(i, j-lags_[k]) * param_->data()[k*param_->m()+i]
+	    + shift_->data()[i];
+	}
+      }
+    }
+
+#undef SMA_AR_GET_VALUE
+    
+    return ret;
+  }
+  
+public:
+  Variable* param_;   // the parameter of ar model
+  Variable* shift_;
+  Variable* var_;     // the data to be build a model on
+  std::vector<int> lags_;
+  int max_lag_;
+  std::unique_ptr<double[]> diff_;
+};
+
+
+
+#endif /* ARWS_H */
--- sma/src/trainc.cpp
+++ sma/src/trainc.cpp
@@ -11,6 +11,7 @@
 #include "fnorm.hpp"
 #include "solver.hpp"
 #include "ar.hpp"
+#include "arws.hpp"
 #include "diff1.hpp"
 
 extern "C" SEXP forecastc(SEXP Y_R,           // data matrix
@@ -33,10 +34,10 @@ extern "C" SEXP forecastc(SEXP Y_R,           // data matrix
   
   std::vector<int> lag;
   for (int i = 0; i < Rf_length(lag_R); i++ ) {
-    lag.push_back(static_cast<int>(REAL(lag_R)[i]));
+    lag.push_back(INTEGER(lag_R)[i]);
   }
   
-  int k = static_cast<int>(*REAL(k_R));
+  int k = static_cast<int>(*INTEGER(k_R));
 
   double lam_nF  = *REAL(lam_nF_R);
   double lam_nX  = *REAL(lam_nX_R);
@@ -44,7 +45,7 @@ extern "C" SEXP forecastc(SEXP Y_R,           // data matrix
   double lam_nT  = *REAL(lam_nT_R);
   double lam_nDF = *REAL(lam_nDF_R);
 
-  int flen = static_cast<int>(*REAL(flen_R));
+  int flen = *INTEGER(flen_R);
   
   /// construct the model
   Variable F(m, k, "F");
@@ -119,6 +120,127 @@ extern "C" SEXP forecastc(SEXP Y_R,           // data matrix
   SET_VECTOR_ELT(ret, 3, rXfor);
   UNPROTECT(1);
 
+  
+  setAttrib(ret, R_NamesSymbol, nms);
+  UNPROTECT(2);
+
+  return ret;
+}
+
+
+extern "C" SEXP forecastwsc(SEXP Y_R,           // data matrix
+			    SEXP k_R,           // the lower rank
+			    SEXP lag_R,         // a vector of lags
+			    SEXP lam_nF_R,      // lambda for ||F||^2
+			    SEXP lam_nX_R,      // lambda for ||X||^2
+			    SEXP lam_AR_R,      // lambda for auto regression
+			    SEXP lam_nT_R,      // lambda for ||Theta||^2
+			    SEXP lam_nDF_R,     // lambda for ||DF||^2
+			    SEXP flen_R) {        // forecast length
+  /// adapt input
+  int m = Rf_nrows(Y_R);
+  int n = Rf_ncols(Y_R);
+
+  Variable Y(m, n, "Y");
+  for (int i = 0; i < m*n; i++)
+    Y.data()[i] = REAL(Y_R)[i];
+  
+  std::vector<int> lag;
+  for (int i = 0; i < Rf_length(lag_R); i++ ) {
+    lag.push_back(INTEGER(lag_R)[i]);
+  }
+  
+  int k = *INTEGER(k_R);
+
+  double lam_nF  = *REAL(lam_nF_R);
+  double lam_nX  = *REAL(lam_nX_R);
+  double lam_AR  = *REAL(lam_AR_R);
+  double lam_nT  = *REAL(lam_nT_R);
+  double lam_nDF = *REAL(lam_nDF_R);
+
+  int flen = *INTEGER(flen_R);
+  
+  /// construct the model
+  Variable F(m, k, "F");
+  Variable X(k, n, "X");
+  Variable T(k, lag.size(), "T");
+  Variable S(k, 1, "S");
+
+  F.init_unif(-1,1);
+  X.init_unif(-1,1);
+  T.init_unif(-1,1);
+  S.init_unif(-1,1);
+  
+  MatFact mf(m, n, k);  
+  mf.add_var(&F);
+  mf.add_var(&X);
+  mf.add_param(&Y);
+
+  AutoRegWithShift ar(&X, &T, &S, lag);
+  ar.set_lambda(lam_AR);
+  
+  FNorm nF(&F);
+  nF.set_lambda(lam_nF);
+
+  FNorm nX(&X);
+  nX.set_lambda(lam_nX);
+
+  FNorm nT(&T);
+  nT.set_lambda(lam_nT);
+
+  Diff1 nDF(&F);
+  nDF.set_lambda(lam_nDF);
+
+  Loss l;
+  l.add_term(&mf);
+  l.add_term(&nF);
+  l.add_term(&nX);
+  l.add_term(&ar);
+  l.add_term(&nT);
+  l.add_term(&nDF);
+
+  // solve the model
+  Solver solver(&l);
+  solver.solve();
+
+  // forecast
+  auto X_forecast = ar.forecast(flen);
+
+  // return value: a list of matrices: F, X and T.
+  SEXP nms = PROTECT(allocVector(STRSXP, 5));
+  SEXP ret = PROTECT(allocVector(VECSXP, 5));
+  
+  SET_STRING_ELT(nms, 0, Rf_mkCharLen("F",1));
+  SEXP rF = PROTECT(Rf_allocMatrix(REALSXP, m, k));
+  std::memcpy(REAL(rF), F.data(), sizeof(double) * m*k);
+  SET_VECTOR_ELT(ret, 0, rF);
+  UNPROTECT(1);
+
+  SET_STRING_ELT(nms, 1, Rf_mkCharLen("X",1));
+  SEXP rX = PROTECT(Rf_allocMatrix(REALSXP, k, n));
+  std::memcpy(REAL(rX), X.data(), sizeof(double) * k*n);
+  SET_VECTOR_ELT(ret, 1, rX);
+  UNPROTECT(1);
+
+  SET_STRING_ELT(nms, 2, Rf_mkCharLen("T",1));
+  SEXP rT = PROTECT(Rf_allocMatrix(REALSXP, k, lag.size()));
+  std::memcpy(REAL(rT), T.data(), sizeof(double) * k*lag.size());
+  SET_VECTOR_ELT(ret, 2, rT);
+  UNPROTECT(1);
+
+  SET_STRING_ELT(nms, 3, Rf_mkCharLen("X.forecast",10));
+  SEXP rXfor = PROTECT(Rf_allocMatrix(REALSXP, X_forecast->m(), X_forecast->n()));
+  std::memcpy(REAL(rXfor), X_forecast->data(),
+	      sizeof(double) * X_forecast->m()*X_forecast->n());
+  SET_VECTOR_ELT(ret, 3, rXfor);
+  UNPROTECT(1);
+
+  SET_STRING_ELT(nms, 4, Rf_mkCharLen("S",1));
+  SEXP rS = PROTECT(Rf_allocMatrix(REALSXP, k, 1));
+  std::memcpy(REAL(rS), S.data(), sizeof(double) * k);
+  SET_VECTOR_ELT(ret, 4, rS);
+  UNPROTECT(1);
+  
   
   setAttrib(ret, R_NamesSymbol, nms);
   UNPROTECT(2);
