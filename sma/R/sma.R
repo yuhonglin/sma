9c8b70cff8b486c2d1abaae04630808b3b75f362
Honglin Yu <honglin.yu@anu.edu.au>
2018-01-04 15:04:25 +1100

    modify function names

--- sma/R/sma.R
+++ sma/R/sma.R
@@ -1,5 +1,20 @@
-train.mf.noshift <- function(Y, k, lag, lam.nF, lam.nX, lam.AR, lam.nT, lam.nDF, flen) {
-    p = .Call("forecastc", Y, as.integer(k), as.integer(lag), as.numeric(lam.nF), as.numeric(lam.nX), as.numeric(lam.AR), as.numeric(lam.nT), as.numeric(lam.nDF), as.integer(flen));
+mf.train.noshift <- function(Y, k,
+                             lag,
+                             lam.nF,
+                             lam.nX,
+                             lam.AR,
+                             lam.nT,
+                             lam.nDF,
+                             flen) {
+    p = .Call("forecastc", Y,
+              as.integer(k),
+              as.integer(lag),
+              as.numeric(lam.nF),
+              as.numeric(lam.nX),
+              as.numeric(lam.AR),
+              as.numeric(lam.nT),
+              as.numeric(lam.nDF),
+              as.integer(flen));
 
     p$forecast = p$F%*%p$X.forecast;
     
@@ -7,8 +22,23 @@ train.mf.noshift <- function(Y, k, lag, lam.nF, lam.nX, lam.AR, lam.nT, lam.nDF,
 }
 
 
-train.mf.shift <- function(Y, k, lag, lam.nF, lam.nX, lam.AR, lam.nT, lam.nDF, flen) {
-    p = .Call("forecastwsc", Y, as.integer(k), as.integer(lag), as.numeric(lam.nF), as.numeric(lam.nX), as.numeric(lam.AR), as.numeric(lam.nT), as.numeric(lam.nDF), as.integer(flen));
+mf.train.shift <- function(Y, k,
+                           lag,
+                           lam.nF,
+                           lam.nX,
+                           lam.AR,
+                           lam.nT,
+                           lam.nDF,
+                           flen) {
+    p = .Call("forecastwsc", Y,
+              as.integer(k),
+              as.integer(lag),
+              as.numeric(lam.nF),
+              as.numeric(lam.nX),
+              as.numeric(lam.AR),
+              as.numeric(lam.nT),
+              as.numeric(lam.nDF),
+              as.integer(flen));
 
     p$forecast = p$F%*%p$X.forecast;
     
@@ -16,7 +46,15 @@ train.mf.shift <- function(Y, k, lag, lam.nF, lam.nX, lam.AR, lam.nT, lam.nDF, f
 }
 
 
-select.hyperparam.mf.shift <- function (Y.train, Y.valid, k, lag, lam.nF, lam.nX, lam.AR, lam.nT, lam.nDF) {
+mf.select.hyperparam.shift <- function (Y.train,
+                                        Y.valid,
+                                        k,
+                                        lag,
+                                        lam.nF,
+                                        lam.nX,
+                                        lam.AR,
+                                        lam.nT,
+                                        lam.nDF) {
     flen = as.numeric(ncol(Y.valid))
     
     card = length(k) * length(lag) * length(lam.nF) * length(lam.nX) * length(lam.AR) * length(lam.nT) * length(lam.nDF);
@@ -32,7 +70,53 @@ select.hyperparam.mf.shift <- function (Y.train, Y.valid, k, lag, lam.nF, lam.nX
                     for (lAR in lam.AR) {
                         for (lT in lam.nT) {
                             for (lDF in lam.nDF) {
-                                res = train.mf.shift(Y.train, iter.k, iter.lag, lF, lX, lAR, lT, lDF, flen)
+                                res = mf.train.shift(Y.train, iter.k, iter.lag, lF, lX, lAR, lT, lDF, flen)
+                                p = list(k       = iter.k,
+                                         lag     = iter.lag,
+                                         lam.nF  = lF,
+                                         lam.nX  = lX,
+                                         lam.AR  = lAR,
+                                         lam.nT  = lT,
+                                         lam.nDF = lDF);
+                                params[[idx]] <- p
+                                mses[idx] = norm(Y.valid - res$forecast, "F")
+                                idx = idx + 1
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    return (params[[which.min(mses)]])
+}
+
+mf.select.hyperparam.noshift <- function (Y.train,
+                                          Y.valid,
+                                          k,
+                                          lag,
+                                          lam.nF,
+                                          lam.nX,
+                                          lam.AR,
+                                          lam.nT,
+                                          lam.nDF) {
+    flen = as.numeric(ncol(Y.valid))
+    
+    card = length(k) * length(lag) * length(lam.nF) * length(lam.nX) * length(lam.AR) * length(lam.nT) * length(lam.nDF);
+    
+    params = vector(mode = "list", length = card)
+    mses   = vector(mode = "numeric", length = card)
+
+    idx = 1;
+    for (iter.k in k) {
+        for (iter.lag in lag) {
+            for (lF in lam.nF) {
+                for (lX in lam.nX) {
+                    for (lAR in lam.AR) {
+                        for (lT in lam.nT) {
+                            for (lDF in lam.nDF) {
+                                res = mf.train.noshift(Y.train, iter.k, iter.lag, lF, lX, lAR, lT, lDF, flen)
                                 p = list(k       = iter.k,
                                          lag     = iter.lag,
                                          lam.nF  = lF,
